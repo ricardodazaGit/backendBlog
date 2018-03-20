8f5fef1530c6e49b313c80b32b9155df571c6405
Daza Vegara <rdaza@indra.es>
2018-03-20 11:38:31 +0100

    first commit

---
+++ src/main/java/es/blog/springBlog/controller/IndexController.java
@@ -0,0 +1,13 @@
+package es.blog.springBlog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+	@RequestMapping("/")
+	public String getIndexPage() {
+		return "index";
+	}
+}
